d7de3e1d4355d929e739ab4853b7fd3b0d433845
Ramiz Ali <ramiz.apk7@gmail.com>
2023-03-20 03:30:19 +0300

    complated assignment

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '');
+  }
+
+  static titleize(string) {
+    let nonCapitalizedWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let words = string.split(' ');
+    let titleizedWords = words.map(function (word, index) {
+      if (index == 0) {
+        return this.capitalize(word);
+      } else {
+        if (nonCapitalizedWords.includes(word)) {
+          return word;
+        } else {
+          return this.capitalize(word);
+        }
+      }
+    }, this);
+    return titleizedWords.join(' ');
+  }
 }
